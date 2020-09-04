89bb67f0e0db9320991d1322f0365ad642d00555
William Martin <wjkmartin@gmail.com>
2020-09-04 13:34:20 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux'
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChessRook, faHryvnia, faGripLines, faGripLinesVertical, faArchway, faSquare, faAnchor, faHouseDamage, faPoop, faBeer, faTshirt} from '@fortawesome/free-solid-svg-icons'
+
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import store from './DataHandlers/redux/store'
+
+import Layout from './components/Layout/Layout'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+library.add(faChessRook,faHryvnia,faGripLines,faGripLinesVertical, faArchway, faSquare, faAnchor, faHouseDamage, faPoop, faBeer, faTshirt)
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <React.StrictMode>
+        <Layout />
+      </React.StrictMode>
+    </Provider>,
+document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
