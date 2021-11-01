282677066f7c792165690b1134f9eb42726c4465
William Martin <wjkmartin@gmail.com>
2021-11-01 06:59:26 -0700

    Add new goblin variants

---
+++ src/Data/actors/monsters/goblins/giantGoblin.js
@@ -0,0 +1,8 @@
+import baseGoblin from "./baseGoblin";
+
+export default {
+    ...baseGoblin,
+    actorName: "Giant Goblin",
+    strength: 16,
+    speed: 2,
+}
--- src/Data/minimaps/burrowDungeon.js
+++ src/Data/minimaps/burrowDungeon.js
@@ -1,12 +1,26 @@
-import burrowEntrance from "../locations/burrowDungeon/burrowEntrance"
-import burrowTunnel from "../locations/burrowDungeon/burrowTunnel"
+import burrowEntrance from '../locations/burrowDungeon/burrowEntrance';
+import burrowTunnel from '../locations/burrowDungeon/burrowTunnel';
 
 export default {
-    name: "burrowDungeon",
-    img: "https://via.placeholder.com/418x345",
-    nodes: [
-        [undefined,undefined,undefined],
-        [undefined, burrowEntrance, burrowTunnel],
-        [undefined,undefined,undefined],
-    ]
-}
+  name: 'burrowDungeon',
+  img: 'https://via.placeholder.com/418x345',
+  nodes: [
+    // prettier-ignore
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, burrowEntrance, burrowTunnel, burrowTunnel, burrowTunnel, burrowTunnel, burrowTunnel, undefined, burrowTunnel, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+  ],
+};
